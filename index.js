59531d50c329f5bcccd5a2b76dd196ea37230596
Pablo Bernal Moreno <pbm1998@hotmail.com>
2022-10-24 14:43:05 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const buttonFunction = () => {
+  document.getElementById('paragraph').innerHTML = 'Me clickeaste'
+}
+
+const imageFunction = () => {
+  document.getElementById('hiddenImage').style.display = 'block'
+}
+
+const resetFunction = () => {
+  window.location.reload()
+}
+
+const boldFunction = () => {
+  const element = document.getElementById('bold')
+  element.style.color = 'red'
+
+  setTimeout(() => {
+    element.style.color = "";
+  }, 500);
+}
+
+const button = document.getElementById('button')
+button.addEventListener('click', buttonFunction)
+
+const image = document.getElementById('image')
+image.addEventListener('click', imageFunction)
+
+const reset = document.getElementById('reset')
+reset.addEventListener('click', resetFunction)
+
+const bold = document.getElementById('bold')
+bold.addEventListener("mouseover", boldFunction)
